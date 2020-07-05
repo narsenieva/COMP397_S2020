b39fde7a02e0b784bc179edfb15c7003bdd2df37
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2020-07-05 14:01:06 -0400

    Scene configuration

---
+++ Scripts/objects/scene.ts
@@ -0,0 +1,16 @@
+module objects {
+    export class Scene extends createjs.Container {
+        // Variables
+        public assetManager: createjs.LoadQueue;
+        // Construactor
+        constructor(assetManager:createjs.LoadQueue) {
+            super();
+            this.assetManager = assetManager;
+        }
+
+        public Start():void{}
+        public Update():void{}
+        public Main():void{}
+    }
+
+}
